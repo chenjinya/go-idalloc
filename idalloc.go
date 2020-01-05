21dcd39b31be8b5b45874f0c668cf5818c77d4de
Jinya Chen <jinyachen@gmail.com>
2020-01-05 20:05:00 +0800

    up: add comment

--- idalloc.go
+++ idalloc.go
@@ -12,19 +12,37 @@ type Idalloc struct {
 	Type string
 }
 
+//缓存ID
 var idalloc_id map[string]uint64
+
+//缓存刷新时间
 var idalloc_timeout map[string]int64
+
+//Debug 模式
+var is_debug bool = false
+
+//启动自增步长
+var boot_auto_incre uint64 = 1000
 
 //同步磁盘间隔
 var idalloc_sync_duration int64 = 1
 
+//缓存文件
 const filePathPrefix = "./idalloc_"
+
+func (self *Idalloc) Debug(b bool) {
+	is_debug = b
+}
 
 func (self *Idalloc) init() {
 	if idalloc_id == nil {
 		idalloc_id = make(map[string]uint64)
 		idalloc_timeout = make(map[string]int64)
 	}
+}
+
+func (self *Idalloc) BootAutoIncre(n uint64) {
+	boot_auto_incre = n
 }
 
 func (self *Idalloc) Gen() (uint64, error) {
@@ -34,7 +52,9 @@ func (self *Idalloc) Gen() (uint64, error) {
 	var file *os.File
 	var err error
 	filePath := filePathPrefix + self.Type
-	fmt.Println("Cache file is", filePath)
+	if is_debug {
+		fmt.Println("[Debug]", "Cache file is", filePath)
+	}
 	if idalloc_id[self.Type] == 0 {
 		file, err = openCacheFile(filePath)
 		defer file.Close()
@@ -47,7 +67,7 @@ func (self *Idalloc) Gen() (uint64, error) {
 		con, _ := reader.ReadString('\n') //读取一行
 		id, _ := strconv.ParseUint(con, 10, 64)
 		//防止ID回流
-		id += 1000
+		id += boot_auto_incre
 		idalloc_id[self.Type] = id
 	}
 
@@ -60,19 +80,21 @@ func (self *Idalloc) Gen() (uint64, error) {
 			file, err = openCacheFile(filePath)
 			defer file.Close()
 			if err != nil {
-				fmt.Println("Open cache file failed", err)
+				fmt.Println("[Error]", "Open cache file failed", err)
 			}
 		}
 		idalloc_timeout[self.Type] = time.Now().Unix()
 
 		if file != nil {
-			fmt.Println("save "+filePath, "is", id_str, "to file")
+			if is_debug {
+				fmt.Println("[Debug]", "save "+filePath, "is", id_str, "to file")
+			}
 			_, err = file.WriteAt([]byte(id_str), 0)
 			if err != nil {
-				fmt.Println("Save cache file failed", err, id_str)
+				fmt.Println("[Error]", "Save cache file failed", err, id_str)
 			}
 		} else {
-			fmt.Println("Need save cache file, but `file` is nill")
+			fmt.Println("[Warn]", "Need save cache file, but `file` is nill")
 		}
 
 	}
@@ -88,13 +110,13 @@ func (self *Idalloc) SyncCacheAll() (bool, error) {
 		file, err := openCacheFile(filePath)
 		defer file.Close()
 		if err != nil {
-			fmt.Println("Open cache file failed", err)
+			fmt.Println("[Error]", "Open cache file failed", err)
 		}
 		id_str := strconv.FormatUint(value, 10)
 		fmt.Println("save "+filePath, "is", id_str, "to file")
 		_, err = file.WriteAt([]byte(id_str), 0)
 		if err != nil {
-			fmt.Println("Save cache file failed", err, id_str)
+			fmt.Println("[Error]", "Save cache file failed", err, id_str)
 		}
 	}
 
@@ -104,9 +126,10 @@ func (self *Idalloc) SyncCacheAll() (bool, error) {
 func openCacheFile(filePath string) (file *os.File, err error) {
 
 	file, err = os.OpenFile(filePath, os.O_RDWR, 0)
-	fmt.Println(file)
 	if err != nil && os.IsNotExist(err) {
-		fmt.Println("File", filePath, "is not Exist, now create it")
+		if is_debug {
+			fmt.Println("[Debug]", "File", filePath, "is not Exist, now create it")
+		}
 		file, err = os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, 0644)
 		if err != nil {
 			fmt.Println(err)
